da9e7a85f811df780d9dab7250b457e56dcf8fe5
Diego-sci01 <diegosantosantana2008@gmail.com>
2025-09-15 20:14:36 +0000

    actualizacion2

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let noun = ['jogger', 'racoon'];
 for (let pronombre of pronoun) {
   for (let adjetivo of adj) {
     for (let sustantivo of noun) {
-      console.log(`${pronombre} ${adjetivo} ${sustantivo}`)
+      console.log(`${pronombre} ${adjetivo} ${sustantivo}.com`)
     }
   }
 }
